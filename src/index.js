41c0a83ea29b6d60b06774243d948dc2dd485746
Ben <benjisgarza@gmail.com>
2020-04-30 12:59:03 -0700

    Event listeners will fire once, but stop after

--- src/index.js
+++ src/index.js
@@ -2,18 +2,28 @@ import Setup from './modules/home';
 import Menu from './modules/menu';
 import Contact from './modules/contact';
 console.log("Index is a go");
-const setTabs = (() => {
+const setTabs = () => {
   let div = document.querySelector('#content');
   let home = document.createElement('label');
   let menu = document.createElement('label');
   let contact = document.createElement('label');
+  home.id = ("home");
   home.classList += "label";
   home.innerText = "Home";
+  menu.id = ("menu");
   menu.innerText = "Menu";
   menu.classList += "label";
   console.log(menu.classList);
+  contact.id = ("contact");
   contact.innerText = "Contact";
   contact.classList += "label";
+  div.append(home, menu, contact);
+};
+setTabs();
+const events = (() => {
+  let home = document.querySelector('#home');
+  let menu = document.querySelector('#menu');
+  let contact = document.querySelector('#contact');
   home.addEventListener('click', function (event) {
     document.querySelector('#content').innerHTML = "";
     setTabs();
@@ -29,8 +39,9 @@ const setTabs = (() => {
     setTabs();
     Contact.initialize();
   });
-  div.append(home, menu, contact);
 })();
+
+
 Setup.test();
 Menu.test();
 Contact.test();
