f88461bcad3009a291a4681362111e531244be0d
Ben <benjisgarza@gmail.com>
2020-05-01 11:51:10 -0700

    pageLoader() now working as intended

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Setup from './modules/home';
+import Home from './modules/home';
 import Menu from './modules/menu';
 import Contact from './modules/contact';
 console.log("Index is a go");
@@ -7,45 +7,41 @@ const setTabs = () => {
   let home = document.createElement('label');
   let menu = document.createElement('label');
   let contact = document.createElement('label');
-  home.id = ("home");
+  home.id = ("Home");
   home.classList += "label";
   home.innerText = "Home";
-  menu.id = ("menu");
+  menu.id = ("Menu");
   menu.innerText = "Menu";
   menu.classList += "label";
   console.log(menu.classList);
-  contact.id = ("contact");
+  contact.id = ("Contact");
   contact.innerText = "Contact";
   contact.classList += "label";
   div.append(home, menu, contact);
 };
 setTabs();
 const events = (() => {
-  let home = document.querySelector('#home');
-  let menu = document.querySelector('#menu');
-  let contact = document.querySelector('#contact');
-  home.addEventListener('click', function (event) {
-    document.querySelector('#content').innerHTML = "";
-    setTabs();
-    Setup.initialize();
-  });
-  menu.addEventListener('click', function (event) {
-    document.querySelector('#content').innerHTML = "";
-    setTabs();
-    Menu.initialize();
-  });
-  contact.addEventListener('click', function (event) {
-    document.querySelector('#content').innerHTML = "";
-    setTabs();
-    Contact.initialize();
-  });
+  const pageLoader = (pageToLoad) => {
+    document.querySelector('#content').innerHTML = ""; //Clear #content div
+    setTabs();// Re-append tabs
+    switch(pageToLoad){
+      case "Home": Home.initialize();break;
+      case "Menu": Menu.initialize();break;
+      case "Contact": Contact.initialize();break;
+    };
+  };
+  document.querySelector('#Home').addEventListener('click', function() {pageLoader(event.target.id);});
+  document.querySelector('#Menu').addEventListener('click', function() {pageLoader(event.target.id);});
+  document.querySelector('#Contact').addEventListener('click', function() {pageLoader(event.target.id);});
 })();
 
 
-Setup.test();
+Home.test();
 Menu.test();
 Contact.test();
-Setup.initialize();
+Home.initialize();
+console.log(typeof(Home));
+//document.querySelector('#content').addEventListener('click', function() {console.log(event.target);});
 
 
 //for tab switching logic
