0f0e642246033ed1d1e676ba21800e59e692ae7a
Ben <benjisgarza@gmail.com>
2020-05-28 17:25:57 -0700

    First part of contact page

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,26 +1,32 @@
-//main div
-  //title div
-  //section
-    // form on one side, article on the other
-    //form
-      //name and email, subject and message
-      //send button
-    //article
-      //div for description
-      //contact info
-      const Menu = (() => {
-     
+const Contact = (() =>
+{
+	const initialize = () =>
+	{
+		//first level
+		let titleDiv = document.createElement("div");
+		let mainSection = document.createElement("section");
+	
+		//mainsection
+		let mainForm = document.createElement("form");
+			//mainForm
+			let nameLabel = document.createElement("label");
+			let nameInput = document.createElement("input");
+			let nameLabel = document.createElement("label");
+			let nameInput = document.createElement("input");
+			let nameLabel = document.createElement("label");
+			let nameInput = document.createElement("input");
+			let nameLabel = document.createElement("label");
+			let nameInput = document.createElement("input");
+		let infoArticle = document.createElement("article");
 
-    
-          const initialize = () => {
-          
-          };
-          const test = () => {
-            console.log('Contact is a go');
-          };
-          return {
-           test,
-           initialize
-          };
-        })();
-        export default Menu;
+	};
+	const test = () =>
+	{
+		console.log('Contact is a go');
+	};
+	return {
+		test,
+		initialize
+	};
+})();
+export default Contact;
