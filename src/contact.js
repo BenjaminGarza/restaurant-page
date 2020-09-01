bfe2118d899db8ed048cd34bc023714f5c9dce73
Ben <benjisgarza@gmail.com>
2020-09-01 16:13:19 -0700

    Creating the function to create the contact form

--- src/contact.js
+++ src/contact.js
@@ -1,10 +1,49 @@
 const loadContactPage = () => {
   const make = (element) => {
-    node = document.createElement('${element}');
-    return node;
+    newElement = document.createElement('${element}');
+    return newElement;
   }
-  let form = document.createElement('form');
+ let contactTitle = make(h2);
+const createForm = () => {
+  let contactForm = make(form);
+
+  let divName = make(div);
+  let labelName = make(label);
+  let inputName = make(input);
+  inputName.setAttribute("type","text");
+  divName.classList.add("form-div");
+  const labelNameContent = document.createTextNode("Name");
+  labelName.appendChild(labelNameContent);
+  divName.appendChild(labelName);
+  divName.appendChild(inputName);
+  contactForm.appendChild(divName);
+
+
+
+  let divEmail = make(div);
+  let labelEmail = make(label);
+  let inputEmail = make(input);
+  const labelEmailContent = document.createTextNode("Name");
+
+  let divSubject = make(div);
+  let labelSubject = make(label);
+  let inputEmail = make(input);
+  const labelNameContent = document.createTextNode("Name");
+
+  let divMessage = make(div);
+  let labelMessage = make(label);
+  let inputMessage= make(input);
+  const labelNameContent = document.createTextNode("Name");
+
+  let asideResponse = make(aside);
+  let labelCheckbox = make(label);
+  let inputCheckbox= make(input);
+  const labelNameContent = document.createTextNode("Name");
+}
  
+let divButton = make(div);
+let buttonCancel = make(button);
+let buttonSubmit = make(button);
 };
 
-export {  };
+export { loadContactPage };
