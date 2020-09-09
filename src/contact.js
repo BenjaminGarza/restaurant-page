d75bfff38886b0708f695154a6b17c356aaf7b19
Ben <benjisgarza@gmail.com>
2020-09-09 10:18:04 -0700

    Contact page finished and styled

--- src/contact.js
+++ src/contact.js
@@ -1,42 +1,60 @@
 const loadContactPage = () => {
- 
+ let contentDiv = document.querySelector('#content');
  let contactTitle = document.createElement('h2');
  contactTitle.innerHTML = 'Contact us';
- console.log(contactTitle);
- document.querySelector('#content').appendChild(contactTitle);
+ contactTitle.classList.add('contact-title');
+ contentDiv.appendChild(contactTitle);
+ let contactForm = document.createElement('form');
+ contactForm.classList.add('contact-form');
 
-const createForm = () => {
-  let contactForm = document.createElement('form');
+
   const createSection = (labelContent, inputType) => {
     let div = document.createElement('div');
     let label = document.createElement('label');
+    label.setAttribute('for', 'input')
     let input = document.createElement('input');
-    input.setAttribute("type","text");
+    input.setAttribute("type",inputType);
     div.classList.add("form-div");
     const labelContentTextNode = document.createTextNode(labelContent);
     label.appendChild(labelContentTextNode);
     div.appendChild(label);
     div.appendChild(input);
-    contact.appendChild(div);
+    contactForm.appendChild(div);
+
+    return( div );
   };
 
   createSection("Name","text");
   createSection("Email","text");
   createSection("Subject","text");
   createSection("Message","textarea");
+  
 
   let asideResponse = document.createElement('aside');
-  asideResponse.classList.add("response");
+  asideResponse.classList.add("response-checkbox");
   let labelCheckbox = document.createElement('label');
-  let inputCheckbox= document.createElement('input');
   const labelMessageContent = document.createTextNode("Would you like a response?");
-}
- 
-let divButton = document.createElement(div);
-let buttonCancel = document.createElement(button);
-let buttonSubmit = document.createElement(button);
+  labelCheckbox.appendChild(labelMessageContent);
+  labelCheckbox.htmlFor = 'input';
+  let inputCheckbox= document.createElement('input');
+  inputCheckbox.type = 'checkbox';
+  asideResponse.appendChild(labelCheckbox);
+  asideResponse.appendChild(inputCheckbox);
+  const checkboxMessage = document.createTextNode("Yes");
+  asideResponse.appendChild(checkboxMessage);
+  contactForm.appendChild(asideResponse);
+
+  contentDiv.appendChild(contactForm);
+let divButton = document.createElement('div');
+let buttonCancel = document.createElement('button');
+buttonCancel.innerHTML = 'Cancel';
+let buttonSubmit = document.createElement('button');
+buttonSubmit.innerHTML = 'Submit';
+buttonCancel.classList.add('contact-button');
+buttonSubmit.classList.add('contact-button');
 divButton.appendChild(buttonCancel);
 divButton.appendChild(buttonSubmit);
+document.querySelector('#content').appendChild(divButton);
 };
 
 export { loadContactPage };
